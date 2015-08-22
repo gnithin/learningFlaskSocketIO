779873f720aaf0c30893ac93ee444807a43b8787
nithin <nithin.linkin@gmail.com>
2015-08-22 13:31:03 +0530

    [app] Working example

--- app/app.py
+++ app/app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
+import logging
+
+logging.basicConfig(
+    filename="my_log.log",
+    level=logging.DEBUG
+)
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route("/chat")
+@app.route("/")
 def index():
     return render_template("index.html")
 
 
-@socketio.on("event")
-def test_connect():
-    pass
-
-@socketio.on_error("event")
-def error_event(e):
-    pass
+@socketio.on("event", namespace="/test")
+def test_connect(msg):
+    logging.debug("Received - " + msg)
+    emit("event", "Hi client!")
 
 @socketio.on_error_default
 def default_err_handler(e):
@@ -23,4 +26,3 @@ def default_err_handler(e):
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0")
-    print "asas"
